182ce903f45e79dffb6327c2e646910812204d8c
hyunjin <s2113@e-mirim.hs.kr>
2023-08-30 12:07:04 +0900

    델리게이터의 람다를 이용해서 다양한 기능 구현

--- DelegatorStudent31/Program.cs
+++ DelegatorStudent31/Program.cs
@@ -10,6 +10,30 @@ namespace DelegatorStudent31
     {
         static void Main(string[] args)
         {
+            Students students = new Students();
+            students.Add(new Student("정현진", 2.0));
+            students.Add(new Student("김자하", 5.0));
+            students.Add(new Student("백설영", 4.0));
+            students.Add(new Student("백언", 2.0));
+            students.Add(new Student("화운", 2.0));
+
+            students.Print();
+            students.Print((student) =>
+            {
+                Console.WriteLine("--------------");
+                Console.WriteLine("이름 : " + student.Name);
+                Console.WriteLine("점수 : " + student.Score);
+            });
+
+            Console.WriteLine("<students>");
+            students.Print((student) =>
+            {
+                Console.WriteLine("<student>");
+                Console.WriteLine("<name>" + student.Name + "</name>");
+                Console.WriteLine("<score>" + student.Score + "</score>");
+                Console.WriteLine("</student>");
+            });
+            Console.WriteLine("</students>");
         }
     }
 }
---
+++ DelegatorStudent31/Students.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatorStudent31
+{
+    internal class Students
+    {
+        private List<Student> students = new List<Student>();
+        public delegate void PrintProcess(Student student);
+
+        public void Add(Student student)
+        {
+            students.Add(student);
+        }
+
+        public void Print(PrintProcess process) {
+            foreach (Student student in students)
+            {
+                process(student);
+            }
+        }
+
+        public void Print()
+        {
+            Print((student) => Console.WriteLine(student));
+        }
+    }
+}
